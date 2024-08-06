90ced33059934612667e18074ddf185130a7b9e5
DonavanC13 <donavanc13@outlook.com>
2024-08-06 12:21:49 -0400

    imported all the files imported and added content needed to populate page in the browser

---
+++ src/index.js
@@ -0,0 +1,45 @@
+import './style.css'
+import { homeFunction } from "./home"
+import { menuFunction } from "./menu";
+import { aboutFunction } from "./about" 
+
+function populatePage(){
+    const content = document.getElementById('content')
+
+    const populateHome = () =>{
+        content.replaceChildren()
+        homeFunction()
+    }
+
+    const populateMenu = () =>{
+        content.replaceChildren()
+        menuFunction()
+    }
+
+    const populateAbout = () =>{
+        content.replaceChildren()
+        aboutFunction()
+    }
+
+    const button = document.querySelectorAll('button')
+    button.forEach(btn =>{
+        btn.addEventListener('click', event =>{
+            button.forEach(btn =>{
+                btn.classList.remove('selected')
+                event.target.classList.add('selected')
+            })
+            if (event.target.id == 'home'){
+                populateHome()
+            }
+            if (event.target.id == 'menu'){
+                populateMenu()
+            }
+            if (event.target.id == 'about'){
+                populateAbout()
+            }
+        })
+    })
+    populateHome()
+}
+
+populatePage()
